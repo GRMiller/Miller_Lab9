7f61658bc2af6971645b969e5ccc30738bfaf4f7
Garrett Miller <garrettralphmiller@gmail.com>
2016-11-10 09:07:45 -0500

    using jQUERY to alter page

---
+++ Scripts/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+	$('#toggle_btn').onclick;
+		$(#index_page).fadeto(
+			background:url(../images/capitol_reef_hero_color.jpg) no-repeat center center fixed;
+
+});
