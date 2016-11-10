dd843bf3c215327b868ae8c1a622d580d89127d1
Garrett Miller <garrettralphmiller@gmail.com>
2016-11-10 15:44:56 -0500

    Colorization and Font-size Increase

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
+ console.log('hello');
 
-	$('#toggle_btn').onclick;
-		$(#index_page).fadeto(
-			background:url(../images/capitol_reef_hero_color.jpg) no-repeat center center fixed;
+	$('.toggle_btn').click(function() {
+		$('.index_page').css({background:'url(images/capitol_reef_hero_color.jpg) no-repeat center center fixed'}, 1000);
+	});
+
+console.log('.index_page')
+
+	$('#about_nav').mouseenter(function() {
+		$(this).css('font-size', '2em')
+	});
+
+	$('#about_nav').mouseleave(function() {
+		$(this).css('font-size', '1.7em')
+	});
+
+	$('#port_nav').mouseenter(function() {
+		$(this).css('font-size', '2em')
+	});
+
+	$('#port_nav').mouseleave(function() {
+		$(this).css('font-size', '1.7em')
+	});
 
 });
